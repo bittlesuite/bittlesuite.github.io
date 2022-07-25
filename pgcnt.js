1b26aaf8635e118ecfee347c7143564dc82de16c
aditi-bhaskar <aditi.bhaskar@gmail.com>
2022-07-25 04:20:20 -0700

    moved likecnt to different file

--- pgcnt.js
+++ pgcnt.js
@@ -1,4 +1,4 @@
-var counterContainer = document.querySelector(".website-counter");
+var counterContainer = document.querySelector(".pg-counter");
 var resetButton = document.querySelector("#reset");
 var visitCount = localStorage.getItem("page_view");
 
@@ -20,18 +20,3 @@ resetButton.addEventListener("click", () => {
 });
 
 //https://contactmentor.com/build-website-visitor-counter-javascript/
-
-
-// my code for like-counter
-
-var likeContainer = document.querySelector(".like-counter")
-var addLike = document.querySelector("#add-like");
-var likeCount = localStorage.getItem("like-cnt");
-
-// Adding onClick event listener
-addLike.addEventListener("click", () => {
-  likeCount = Number(likeCnt) + 1;
-  localStorage.setItem("like-cnt", likeCount);
-  likeContainer.innerHTML = likeCount;
-});
-
