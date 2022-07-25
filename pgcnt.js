0140c959ddc86ec55e971c3f125488842e396bdc
aditi-bhaskar <aditi.bhaskar@gmail.com>
2022-07-25 04:16:15 -0700

    added like counter too

--- pgcnt.js
+++ pgcnt.js
@@ -20,3 +20,18 @@ resetButton.addEventListener("click", () => {
 });
 
 //https://contactmentor.com/build-website-visitor-counter-javascript/
+
+
+// my code for like-counter
+
+var likeContainer = document.querySelector(".like-counter")
+var addLike = document.querySelector("#add-like");
+var likeCount = localStorage.getItem("like-cnt");
+
+// Adding onClick event listener
+addLike.addEventListener("click", () => {
+  likeCount = Number(likeCnt) + 1;
+  localStorage.setItem("like-cnt", likeCount);
+  likeContainer.innerHTML = likeCount;
+});
+
