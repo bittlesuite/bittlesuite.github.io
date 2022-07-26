919184134c0345581d3ddb654b25ccd4242023ee
aditi-bhaskar <aditi.bhaskar@gmail.com>
2022-07-26 00:49:02 -0700

    changed like count to be stored in txt

--- pgcnt.js
+++ pgcnt.js
@@ -24,9 +24,26 @@ resetButton.addEventListener("click", () => {
 
 
 /// for like counter ---
+
+openFile=fopen(getFilePath(),0);
+
+var readString = fread(openFile,flength(file)) ;
+document.getElementById("like").innerHTML = readString;
+function myFunction() {
+    var read = fread(openFile,flength(file)) ;
+    read = int(read) + 1 ;
+
+    editFile = fopen("likecnt.txt", 3);// opens the file for writing
+    fwrite(file, read);// str is the content that is to be written into the file.
+
+    document.getElementById("like").innerHTML = read;
+}
+
+/*
 var clicks = 0;
 document.getElementById("like").innerHTML = clicks;
 function myFunction() {
     clicks += 1;
     document.getElementById("like").innerHTML = clicks;
 }
+*/
