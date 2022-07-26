74c4d765672a756ac5776b88c0e1ef6861e43e78
aditi-bhaskar <aditi.bhaskar@gmail.com>
2022-07-26 07:13:40 -0700

    tried different method of passing var

--- bittlesuite.js
+++ bittlesuite.js
@@ -278,8 +278,8 @@ function myFunction() {
     var copyText = game_play ;//document.getElementById("myInput");
   
     /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+    // copyText.select();
+    // copyText.setSelectionRange(0, 99999); /* For mobile devices */
   
      /* Copy the text inside the text field */
     navigator.clipboard.writeText(copyText.value);
