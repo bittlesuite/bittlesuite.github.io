02caf8d75a630187c1b03e18d3c6c2ae57e8d174
aditi-bhaskar <aditi.bhaskar@gmail.com>
2022-07-26 07:09:08 -0700

    new method of copying

--- bittlesuite.js
+++ bittlesuite.js
@@ -273,6 +273,22 @@ const animateCSS = (element, animation, prefix = 'animate__') =>
 });
 
 
+function myFunction() {
+    /* Get the text field */
+    var copyText = game_play ;//document.getElementById("myInput");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+  
+    /* Alert the copied text */
+    toastr.success("Copied the text: " + copyText.value);
+}
+
+
 //////////////////////////////////////old logic in "checkGuess()" for loop
 
         //MY V3 -- accurate feedback
