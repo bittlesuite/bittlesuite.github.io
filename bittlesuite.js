6f51654819d0596809c6154d8021c43944da9268
aditi-bhaskar <aditi.bhaskar@gmail.com>
2022-07-26 07:30:58 -0700

    added error message

--- bittlesuite.js
+++ bittlesuite.js
@@ -278,13 +278,14 @@ function copyTextFromElement(elementID) {
     let element = document.getElementById(elementID); //select the element
     let elementText = element.textContent; //get the text content from the element
     copyText(elementText); //use the copyText function below
-  }
+}
   
 //If you only want to put some Text in the Clipboard just use this function
 // and pass the string to copied as the argument.
 function copyText(text) {
     navigator.clipboard.writeText(text);
-  }
+    toastr.success("copied!")
+}
 
 //////////////////////////////////////old logic in "checkGuess()" for loop
 
