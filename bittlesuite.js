268625dcb906d259c90a5ad658d0c37a5f204e62
aditi-bhaskar <aditi.bhaskar@gmail.com>
2022-07-26 08:25:49 -0700

    changed copy stuff for now; need to copy manually.

--- bittlesuite.js
+++ bittlesuite.js
@@ -35,7 +35,7 @@ console.log(rightGuessString)
 
 //giving string of game play 
 document.getElementById("game").innerHTML = "";
-var game_play = "" ;
+var game_play = "Share your game! <br/>" ;
 if (mode == 1) {
     game_play += "bittle! <br/>" ;
 } else if (mode == 4) {
