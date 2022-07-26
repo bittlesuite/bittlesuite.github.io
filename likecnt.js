bcbcd802ea337c95c1b564f6fcc1065cb79a2d16
aditi-bhaskar <aditi.bhaskar@gmail.com>
2022-07-25 22:21:48 -0700

    fixed like counter?

--- likecnt.js
+++ likecnt.js
@@ -5,6 +5,11 @@ var likeContainer = document.querySelector(".like-counter")
 var addLike = document.querySelector("#add-like");
 var likeCount = localStorage.getItem("like-cnt");
 
+likeCount = 1;
+localStorage.setItem("like-cnt", 1);
+counterContainer.innerHTML = likeCount;
+
+
 // Adding onClick event listener
 addLike.addEventListener("click", () => {
   likeCount = Number(likeCnt) + 1;
